d05de3d6abea340a444c4c1c71bcdbc9a07be5d6
anatawa12 <anatawa12@icloud.com>
2021-05-07 22:19:11 +0900

    エラー処理のテスト

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
+use warp::http::StatusCode;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -7,10 +8,35 @@ async fn main() -> std::io::Result<()> {
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .and_then(|_name| async move { 
+            if false {
+                Ok(warp::reply())
+            } else {
+                Err(warp::reject::custom(AppError::AuthFail))
+            }
+        })
+        .recover(handle_rejection);
 
     warp::serve(hello)
         .run(([0, 0, 0, 0], 3030))
         .await;
     Ok(())
+}
+
+#[derive(Debug)]
+enum AppError {
+    AuthFail
+}
+
+impl warp::reject::Reject for AppError {
+}
+
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(err) = err.find::<AppError>() {
+        return match err {
+            AppError::AuthFail =>
+                Ok(warp::reply::with_status(warp::reply(), StatusCode::UNAUTHORIZED))
+        }
+    }
+    return Err(err)
 }
