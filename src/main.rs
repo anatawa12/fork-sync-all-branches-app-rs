47656d2c5bb22bdb9fd00b78ffb41155d6a3a0bf
anatawa12 <anatawa12@icloud.com>
2021-05-07 21:07:57 +0900

    とりあえずwarpを使ってみる

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
-fn main() {
+use warp::Filter;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 3030))
+        .await;
+    Ok(())
 }
